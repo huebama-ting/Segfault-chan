36d730e23ca156db03d99c00a4d728c084b658a2
Jacky Liang <jliang06@uoguelph.ca>
2019-12-22 00:14:14 -0500

    Updated database call to accept JP name searching

--- src/main/java/com/github/huebama_ting/segfault_chan/ServantQuery.java
+++ src/main/java/com/github/huebama_ting/segfault_chan/ServantQuery.java
@@ -31,11 +31,12 @@ public class ServantQuery {
         ArrayList<Servant> servantList = new ArrayList<>();
 
         while (rs.next()) {
-            Servant serv = new Servant(rs.getShort("id"), rs.getString("name"), rs.getString("class"),
-                                       rs.getInt("hp"), rs.getShort("atk"), rs.getString("traits"),
-                                       rs.getString("illust"), rs.getString("cv"), rs.getString("align"),
-                                       rs.getString("ht_wt"), rs.getString("gender"), rs.getString("nick"),
-                                       rs.getString("attrib"), rs.getString("rarity"), rs.getString("img"));
+            Servant serv = new Servant(rs.getShort("id"), rs.getString("name_en"), rs.getString("name_jp"),
+                                       rs.getString("class"), rs.getInt("hp"), rs.getShort("atk"),
+                                       rs.getString("traits"), rs.getString("illust"), rs.getString("cv"),
+                                       rs.getString("align"), rs.getString("ht_wt"), rs.getString("gender"),
+                                       rs.getString("nick"), rs.getString("attrib"), rs.getString("rarity"),
+                                       rs.getString("img"));
 
             servantList.add(serv);
         }
@@ -52,7 +53,8 @@ public class ServantQuery {
             return "SELECT * FROM " + conn.getTable() + " WHERE id = " + search;
         }
 
-        return "SELECT * FROM " + conn.getTable() +  " WHERE name = '" + search + "' OR name LIKE '%" + search +
-               "%' OR nick " + "LIKE '%" + search + "%'";
+        return "SELECT * FROM " + conn.getTable() +  " WHERE name_en = '" + search + "' OR name_en LIKE '%" + search +
+                "%' OR name_jp = '" + search + "' OR name_jp LIKE '%" + search + "%' OR nick " + "LIKE '%" + search +
+                "%'";
     }
 }
