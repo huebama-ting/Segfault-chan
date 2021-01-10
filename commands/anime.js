b842faf02f85758da041fc55c89d6708cf138885
Jacky Liang <jliang06@uoguelph.ca>
2021-01-10 00:41:02 -0500

    Move common functions to dedicated helper files

--- commands/anime.js
+++ commands/anime.js
@@ -1,6 +1,7 @@
 const { apiUrl } = require('../config.json');
+const { handleResponse, logError, sendReply, formatInfo, formatDescription, formatGenres } = require('../common/helpers');
+
 const discord = require('discord.js');
-const chalk = require('chalk');
 const fetch = require('node-fetch');
 
 module.exports = {
@@ -11,52 +12,31 @@ module.exports = {
     parameters: 1,
     usage: '<name of anime>',
     async execute(msg, args) {
-        const handleResponse = (response) => {
-            return response.json().then(json => {
-                return response.ok ? json : Promise.reject(json);
-            });
-        };
-        const sendReply = (info) => {
-            console.log(info);
+        const makeEmbed = (info) => {
             const embed = new discord.MessageEmbed()
-                .setColor('#FF0000')
-                .setTitle(info.data.Media.title.english ?? info.data.Media.title.romaji ?? info.data.Media.title.native)
-                .setDescription(info.data.Media.title.romaji + ' / ' + info.data.Media.title.native)
-                .setThumbnail(info.data.Media.coverImage.medium)
-                .addFields(
-                    { name: '\u200b', value: '\u200b', inline: false },
-                    { name: 'Description', value: formatDescription(info.data.Media.description), inline: false },
-                    { name: 'Format', value: formatInfo(info.data.Media.format), inline: true },
-                    { name: 'Episodes', value: formatInfo(info.data.Media.episodes), inline: true },
-                    { name: 'Status', value: formatInfo(info.data.Media.status), inline: true },
-                    { name: 'Season', value: info.data.Media.season + ' ' + info.data.Media.seasonYear, inline: true },
-                    { name: 'Studio', value: formatStudio(info.data.Media.studios.nodes), inline: true },
-                    { name: 'Genres', value: formatGenres(info.data.Media.genres), inline: true },
-                    { name: 'Average Score', value: formatInfo(info.data.Media.averageScore) !== 'N/A' ? formatInfo(info.data.Media.averageScore) + '%' : formatInfo(info.data.Media.averageScore), inline: true },
-                    { name: 'Source', value: formatInfo(info.data.Media.source), inline: true }
-                );
-
-            try {
-                msg.channel.send(embed);
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        const logError = (error) => {
-            console.log(error);
-        };
-        const formatDescription = (description) => {
-            if (description === null) {
-                return 'N/A';
-            }
-
-            return description.replace(/(<br>)+/g, '\u000a').trim();
+            .setColor('#FF0000')
+            .setTitle(info.data.Media.title.english ?? info.data.Media.title.romaji ?? info.data.Media.title.native)
+            .setDescription(info.data.Media.title.romaji + ' / ' + info.data.Media.title.native)
+            .setThumbnail(info.data.Media.coverImage.medium)
+            .addFields(
+                { name: '\u200b', value: '\u200b', inline: false },
+                { name: 'Description', value: formatDescription(info.data.Media.description), inline: false },
+                { name: 'Format', value: formatInfo(info.data.Media.format), inline: true },
+                { name: 'Episodes', value: formatInfo(info.data.Media.episodes), inline: true },
+                { name: 'Status', value: formatInfo(info.data.Media.status), inline: true },
+                { name: 'Season', value: info.data.Media.season + ' ' + info.data.Media.seasonYear, inline: true },
+                { name: 'Studio', value: formatStudio(info.data.Media.studios.nodes), inline: true },
+                { name: 'Genres', value: formatGenres(info.data.Media.genres), inline: true },
+                { name: 'Average Score', value: formatInfo(info.data.Media.averageScore) !== 'N/A' ? formatInfo(info.data.Media.averageScore) + '%' : formatInfo(info.data.Media.averageScore), inline: true },
+                { name: 'Source', value: formatInfo(info.data.Media.source), inline: true }
+            );
+            sendReply(msg, embed);
         };
         const formatStudio = (array) => {
             let formattedString = '';
 
-            for (const string of array) {
-                formattedString += string.name + ', ';    
+            for (const str of array) {
+                formattedString += str.name + ', ';    
             }
 
             formattedString = formattedString.slice(0, formattedString.length - 2);
@@ -67,28 +47,6 @@ module.exports = {
 
             return formattedString;
         };
-        const formatGenres = (array) => {
-            let formattedString = '';
-
-            for (const string of array) {
-                formattedString += string + ', ';    
-            }
-
-            formattedString = formattedString.slice(0, formattedString.length - 2);
-
-            if (formattedString.length === 0 || formattedString === ' ') {
-                formattedString = 'N/A';
-            }
-
-            return formattedString;
-        };
-        const formatInfo = (data) => {
-            if (data === null) {
-                return 'N/A';
-            }
-
-            return data;
-        }
         const query = `
         query getAnimeByName ($search: String) {
             Media (search: $search, type: ANIME) {
@@ -132,6 +90,6 @@ module.exports = {
             })
         };
 
-        fetch(apiUrl, options).then(handleResponse).then(sendReply).catch(logError);
+        fetch(apiUrl, options).then(handleResponse).then(makeEmbed).catch(logError);
     }
 };
---
+++ common/helpers.js
@@ -0,0 +1,58 @@
+function handleResponse(response) {
+    return response.json().then(json => {
+        return response.ok ? json : Promise.reject(json);
+    });
+};
+
+function logError(error) {
+    console.log(error);
+};
+
+function sendReply(msg, embed) {
+    try {
+        msg.channel.send(embed);
+    } catch (err) {
+        msg.client.extLog.error(chalk.red(err));
+    }
+};
+
+function formatInfo(str) {
+    if (str === null) {
+        return 'N/A';
+    }
+
+    return str;
+}
+
+function formatDescription(description) {
+    if (description === null) {
+        return 'N/A';
+    }
+
+    return description.replace(/(<br>)+/g, '\u000a').trim();
+};
+
+function formatGenres(array) {
+    let formattedString = '';
+
+    for (const str of array) {
+        formattedString += str + ', ';    
+    }
+
+    formattedString = formattedString.slice(0, formattedString.length - 2);
+
+    if (formattedString.length === 0 || formattedString === ' ') {
+        formattedString = 'N/A';
+    }
+
+    return formattedString;
+};
+
+module.exports = {
+    handleResponse,
+    logError,
+    sendReply,
+    formatInfo,
+    formatDescription,
+    formatGenres
+}
