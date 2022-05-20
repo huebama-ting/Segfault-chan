aa37d3182766d4d274cede26e6d147a7e0084994
Jacky Liang <jliang06@uoguelph.ca>
2022-05-20 17:23:22 -0400

    Add support for up to 10 definitions per Urban Dictionary search term

--- Segfault-chan.Bot/src/commands/urban-dictionary.command.ts
+++ Segfault-chan.Bot/src/commands/urban-dictionary.command.ts
@@ -1,12 +1,25 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
+import {
+  bold,
+  italic,
+  SlashCommandBuilder
+} from '@discordjs/builders';
 import dayjs from 'dayjs';
-import { MessageEmbed } from 'discord.js';
+import {
+  MessageActionRow,
+  MessageButton,
+  MessageEmbed
+} from 'discord.js';
+import { v4 as uuidv4 } from 'uuid';
 
 import { apiBaseUrl } from 'src/config/config.json';
 import { Definition } from 'src/models/urban-dictionary/definition.model';
 import { httpService } from 'src/services/http/http.service';
 import { removeCharacters, trim } from 'src/shared/utils';
+
 import { Command } from './models/command.model';
+
+const URBAN_DICTIONARY_ENDPOINT = 'api/v1/urbanDictionary/define';
+const REACTION_TIMEOUT = 60000;
 
 export const command: Command = {
   data: new SlashCommandBuilder()
@@ -19,38 +32,95 @@ export const command: Command = {
   async execute(interaction): Promise<void> {
     const term = interaction.options.getString('term') as string;
     const query = new URLSearchParams({ term });
+    const response = await httpService.get<Definition[]>(`${apiBaseUrl}/${URBAN_DICTIONARY_ENDPOINT}?${query}`);
 
-    const response = await httpService.get<Definition[]>(`${apiBaseUrl}/api/v1/urbanDictionary/define?${query}`);
-    
     if (response.length === 0) {
-      return await interaction.reply(`No results found for **${term}**.`);
+      return await interaction.reply(`No results found for ${italic(bold(term))}.`);
     }
     
-    const result = response[0];
+    let embed = createEmbed(term, response, 0, response.length);
+    const forwardUuid = uuidv4();
+    const backwardUuid = uuidv4();
+    const buttons = new MessageActionRow()
+      .addComponents(
+        new MessageButton()
+          .setCustomId(backwardUuid)
+          .setLabel('◀️')
+          .setStyle('PRIMARY'),
+          new MessageButton()
+          .setCustomId(forwardUuid)
+          .setLabel('▶️')
+          .setStyle('PRIMARY')
+      );
 
-    if (result !== undefined) {
-      const embed = new MessageEmbed()
-        .setTitle(term)
-        .setURL(result.permalink)
-        .setAuthor({
-          name: result.author
-        })
-        .setTimestamp(dayjs(result.written_on).toDate())
-        .addFields(
-          {
-            name: 'Definition',
-            value: removeCharacters(result.definition, '[]')
-          },
-          {
-            name: 'Example',
-            value: trim(removeCharacters(result.example, '[]'), 1024)
-          }, 
-          {
-            name: 'Rating',
-            value: `${result.thumbs_up} thumbs up.\n${result.thumbs_down} thumbs down.`
-          });
+    interaction.reply({ components: [buttons], embeds: [embed] });
 
-      interaction.reply({ embeds: [embed] });
-    }
+    const collector = interaction.channel?.createMessageComponentCollector({ componentType: 'BUTTON', time: REACTION_TIMEOUT });
+    let index = 0;
+
+    collector?.on('collect', async (buttonInteraction) => {
+      if (buttonInteraction.customId === forwardUuid) {
+        index++;
+      } else if (buttonInteraction.customId === backwardUuid) {
+        index--;
+      } else {
+        return;
+      }
+
+      if (index === response.length) {
+        index = 0;
+      } else if (index < 0) {
+        index = response.length - 1;
+      }
+
+      embed = createEmbed(term, response, index, response.length);
+
+      await buttonInteraction.update({ components: [buttons], embeds: [embed] });
+    });
+
+    collector?.on('end', async () => {
+      await interaction.editReply({ components: [] });
+    });
   }
 };
+
+function calculateRating(thumbsUp: number, thumbsDown: number): number {
+  const total = thumbsUp + thumbsDown;
+
+  if (total === 0) {
+    return 0;
+  }
+
+  return (thumbsUp / total) * 100;
+}
+
+function createEmbed(term: string, results: Definition[], index: number, size: number): MessageEmbed {
+  return new MessageEmbed()
+    .setTitle(term)
+    .setURL(results[index].permalink)
+    .setAuthor({
+      name: results[index].author
+    })
+    .setTimestamp(dayjs(results[index].written_on).toDate())
+    .addFields(
+      {
+        name: 'Definition',
+        value: trim(removeCharacters(results[index].definition, '[]'), 1024)
+      },
+      {
+        name: 'Example',
+        value: trim(removeCharacters(results[index].example, '[]'), 1024)
+      }, 
+      {
+        name: 'Rating',
+        value: `${results[index].thumbs_up} ${results[index].thumbs_up === 1 ? 'thumb' : 'thumbs'} up
+          ${results[index].thumbs_down} ${results[index].thumbs_down === 1 ? 'thumb' : 'thumbs'} down`
+      },
+      {
+        name: 'Approval',
+        value: `${calculateRating(results[index].thumbs_up, results[index].thumbs_down).toFixed(2)}%`
+      }
+    )
+    .setColor('RANDOM')
+    .setFooter({ text: `${index + 1}/${size}` });
+}
