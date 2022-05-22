90486a860caf7d1426cb06e4ca42fc99ac91711a
Jacky Liang <jliang06@uoguelph.ca>
2022-05-21 23:52:42 -0400

    Add constant for cat API endpoint

--- Segfault-chan.Bot/src/commands/cat.command.ts
+++ Segfault-chan.Bot/src/commands/cat.command.ts
@@ -6,12 +6,14 @@ import { httpService } from 'src/services/http/http.service';
 
 import { Command } from './models/command.model';
 
+const RANDOM_CAT_ENDPOINT = 'api/v1/randomCat';
+
 export const command: Command = {
   data: new SlashCommandBuilder()
     .setName('cat')
     .setDescription('Gets a random image of a cat.'),
   async execute(interaction): Promise<void> {
-    const response = await httpService.get<RandomCat>(`${apiBaseUrl}/api/v1/randomCat`);
+    const response = await httpService.get<RandomCat>(`${apiBaseUrl}/${RANDOM_CAT_ENDPOINT}`);
     const catPicture = response.url;
 
     await interaction.reply(catPicture);
